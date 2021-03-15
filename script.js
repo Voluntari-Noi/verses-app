6fc8f36f5a3b41b1984c105aaf99769993d03683
GhitaB <ghitabizau27@gmail.com>
2021-03-15 19:04:49 +0200

    [refs #38] Update books

--- script.js
+++ script.js
@@ -12,7 +12,20 @@ $("document").ready(function () {
     window.consecutive_fails = 0;
     window.bonus_points = 0;
     window.hint_used = false;
-    window.all_books = ['Geneza', 'Exod', 'Levitic', 'Numeri', 'Deuteronom', 'Iosua', 'Judecători', 'Rut', '1 Samuel', '2 Samuel', '1 Împăraţi', '2 Împăraţi', '1 Cronici', '2 Cronici', 'Ezra', 'Neemia', 'Estera', 'Iov', 'Psalmii', 'Proverbe', 'Eclesiastul', 'Cântarea cântărilor', 'Isaia', 'Ieremia', 'Plângerile lui Ieremia', 'Ezechiel', 'Daniel', 'Osea', 'Ioel', 'Amos', 'Obadia', 'Iona', 'Mica', 'Naum', 'Habacuc', 'Țefania', 'Hagai', 'Zaharia', 'Maleahi', 'Matei', 'Marcu', 'Luca', 'Ioan', 'Faptele Apostolilor', 'Romani', '1 Corinteni', '2 Corinteni', 'Galateni', 'Efeseni', 'Filipeni', 'Coloseni', '1 Tesaloniceni', '2 Tesaloniceni', '1 Timotei', '2 Timotei', 'Tit', 'Filimon', 'Evrei', 'Iacov', '1 Petru', '2 Petru', '1 Ioan', '2 Ioan', '3 Ioan', 'Iuda', 'Apocalipsa'];
+    window.all_books = [
+      'Geneza', 'Exodul', 'Levitic',
+      'Numeri', 'Deuteronom', 'Iosua', 'Judecători',
+      'Rut', '1 Samuel', '2 Samuel', '1 Împăraţi', '2 Împăraţi',
+      '1 Cronici', '2 Cronici', 'Ezra', 'Neemia', 'Estera',
+      'Iov', 'Psalmii', 'Proverbe', 'Eclesiastul', 'Cântarea cântărilor',
+      'Isaia', 'Ieremia', 'Plângerile lui Ieremia', 'Ezechiel', 'Daniel',
+      'Osea', 'Ioel', 'Amos', 'Obadia', 'Iona', 'Mica', 'Naum', 'Habacuc',
+      'Țefania', 'Hagai', 'Zaharia', 'Maleahi', 'Matei', 'Marcu', 'Luca',
+      'Ioan', 'Faptele Apostolilor', 'Romani', '1 Corinteni', '2 Corinteni',
+      'Galateni', 'Efeseni', 'Filipeni', 'Coloseni', '1 Tesaloniceni',
+      '2 Tesaloniceni', '1 Timotei', '2 Timotei', 'Tit', 'Filimon', 'Evrei',
+      'Iacov', '1 Petru', '2 Petru', '1 Ioan', '2 Ioan', '3 Ioan',
+      'Iuda', 'Apocalipsa'];
     // window.texts = [ ]; - see verses.js
     window.current_text = "aaa";
     window.old_text = "aaa";
